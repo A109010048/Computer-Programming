0401ce0bf05df42e3ea3c03d4460d9ba836ffa97
Jessie B <79630573+chieh28@users.noreply.github.com>
2023-03-13 12:42:30 +0800

    Add files via upload

---
+++ ex2.cs
@@ -0,0 +1,30 @@
+﻿int score;
+Console.WriteLine("請輸入成績:");
+string s = Console.ReadLine();
+score = int.Parse(s);
+
+if (score >= 0 && score <= 59)
+{
+    Console.WriteLine("E");
+}
+if (score >= 60 && score <= 69)
+{
+    Console.WriteLine("D");
+}
+if (score >= 70 && score <= 79)
+{
+    Console.WriteLine("C");
+}
+if (score >= 80 && score <= 89)
+{
+    Console.WriteLine("B");
+}
+if (score >= 90 && score <= 100)
+{
+    Console.WriteLine("A");
+}
+if (score < 0 || score > 100)
+{
+    Console.WriteLine("輸入錯誤");
+}
+Console.Read();
