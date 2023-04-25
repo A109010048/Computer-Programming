04f485ca870f854f20278c4fb63dd6c2de7e208b
Jessie B <79630573+chieh28@users.noreply.github.com>
2023-04-25 18:31:23 +0800

    Add files via upload

---
+++ ex6_foreach/a109010048ex8/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] pw = new int[10] { 123, 456, 789, 147, 258, 369, 159, 357, 951, 753 };
+Console.WriteLine("請輸入密碼:");
+int input = int.Parse(Console.ReadLine());
+foreach (int i in pw)
+{
+    if (i == input)
+    {
+        Console.WriteLine("驗證成功");
+        break;
+    }
+    if (i == 753 && i != input)
+        Console.WriteLine("驗證失敗");
+}
+Console.Read();
