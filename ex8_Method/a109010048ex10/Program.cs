dab03c2abf5422511805994a08cc90a78eacd29f
Jessie B <79630573+chieh28@users.noreply.github.com>
2023-05-08 12:46:32 +0800

    Add files via upload

---
+++ ex8_Method/a109010048ex10/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static double CircleArea(double r)
+    {
+        double a = r * r * 3.14;
+        return a;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("請輸入第一個半徑:");
+        double r1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("請輸入第二個半徑:");
+        double r2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("請輸入第三個半徑:");
+        double r3 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("當半徑是{0}時，圓面積是{1}", r1, CircleArea(r1));
+        Console.WriteLine("當半徑是{0}時，圓面積是{1}", r2, CircleArea(r2));
+        Console.WriteLine("當半徑是{0}時，圓面積是{1}", r3, CircleArea(r3));
+    }
+
+}
