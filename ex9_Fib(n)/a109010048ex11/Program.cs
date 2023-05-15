c0c7a8b5f7273f1097fce65ea7ddd0310445fbe3
Jessie B <79630573+chieh28@users.noreply.github.com>
2023-05-15 13:19:46 +0800

    Add files via upload

---
+++ ex9_Fib(n)/a109010048ex11/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static int fibonacci(ref int x)
+    {
+        if (x == 0) return 0;
+        if (x == 1) return 1;
+        if (x == 2) return 1;
+
+        int prev1 = 1;
+        int prev2 = 1;
+        int cur = 0;
+
+        for(int i = 3; i <= x ;i++)
+        {
+            cur = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = cur;
+        }
+        
+        return cur;
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("input n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = fibonacci(ref n);
+        Console.WriteLine("\nFin({0}) = {1}", n, result);
+    }
+}
